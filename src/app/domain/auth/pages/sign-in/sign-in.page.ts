401115dc3c66971cd92b84623238d0d16f4dad56
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-03-03 07:26:50 -0300

    ✨ feat(sing-in page ):  use reactive forms and attach events in icons

--- src/app/domain/auth/pages/sign-in/sign-in.page.ts
+++ src/app/domain/auth/pages/sign-in/sign-in.page.ts
@@ -2,24 +2,54 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
+  OnInit,
   ViewChild,
 } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseService } from 'src/app/services/firebase/firebase.service';
+import { CustomValidators } from 'src/app/widgets/show-validation-error/validators/custom-validators';
 
 @Component({
   templateUrl: './sign-in.page.html',
   styleUrls: ['./sign-in.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignInPage {
+export class SignInPage implements OnInit {
   @ViewChild('passwordInput') input: ElementRef;
   showPassword: boolean;
-
+  singInGroup: FormGroup;
   constructor(
     private readonly firebase: FirebaseService,
     private readonly router: Router
   ) {}
+
+  ngOnInit(): void {
+    this.createSingInGroup();
+  }
+
+  createSingInGroup() {
+    this.singInGroup = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        CustomValidators.email,
+      ]),
+      password: new FormControl(null, [
+        Validators.minLength(8),
+        Validators.required,
+      ]),
+    });
+  }
+  singInWithEmailAndPassword() {
+    if (this.singInGroup.invalid) return;
+    const { email, password } = this.singInGroup.value as {
+      email: string;
+      password: string;
+    };
+    this.firebase.signInWithEmailAndPassword(email, password).subscribe(() => {
+      this.router.navigate(['/home']);
+    });
+  }
 
   signInWithGoogle() {
     this.firebase.signInWithGoogle().subscribe((result) => {
