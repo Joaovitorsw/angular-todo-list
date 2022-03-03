0f6cafc18f56dd535462b7cee02b70d443f61e3b
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2022-03-03 07:27:16 -0300

    ✨ feat(auth domain module): update imports

--- src/app/domain/auth/auth.domain.module.ts
+++ src/app/domain/auth/auth.domain.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ShowValidationErrorWidgetModule } from 'src/app/widgets/show-validation-error/show-validation-error-widget.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { SignInPage } from './pages/sign-in/sign-in.page';
 import { SignUpPage } from './pages/sign-up/sign-up.page';
-
 @NgModule({
   declarations: [AuthComponent, SignUpPage, SignInPage],
   imports: [
@@ -24,6 +26,9 @@ import { SignUpPage } from './pages/sign-up/sign-up.page';
     MatInputModule,
     CommonModule,
     MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ShowValidationErrorWidgetModule,
   ],
 })
 export class AuthDomainModule {}
